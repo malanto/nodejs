53118c67af599b25479aa2bb895e7fd3b1d9f96f
malanto <78157677+malanto@users.noreply.github.com>
2024-01-25 05:09:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
-const port= process.env.PORT||3000;
+const uuid= (process.env.UUID||'a7816bb9-e4ca-4d46-9b56-657161eead47').replace(/-/g, "");
+const port= process.env.PORT||80;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
 wss.on('connection', ws=>{
